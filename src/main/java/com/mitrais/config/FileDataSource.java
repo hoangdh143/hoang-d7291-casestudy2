2918c5611854d6bb58850bd4346808569e0dbd87
HoangDH <huuhoang.dao@gmail.com>
2019-09-25 10:56:25 +0700

    Fixed Datasource config

--- src/main/java/com/mitrais/config/FileDataSource.java
+++ src/main/java/com/mitrais/config/FileDataSource.java
@@ -12,10 +12,13 @@ import java.util.stream.Collectors;
 
 public class FileDataSource implements DataSource<Account> {
     private static final String FILE_URL = "Accounts.csv";
+    private static final int MAX_ROW = 20;
+
     private AccountValidationContext context;
 
     @Override
     public Map<String, Account> loadData() throws DataSourceException {
+
         Map<String, Account> map;
         File inputF = new File(FILE_URL);
 
@@ -28,7 +31,7 @@ public class FileDataSource implements DataSource<Account> {
             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
             map = br.lines().map(this::mapToAccount).collect(Collectors.toMap(Account::getAccountNumber, Function.identity()));
 
-            if (map.size() < 20) throw new DataSourceException("File should not have less than 20 rows");
+            if (map.size() < MAX_ROW) throw new DataSourceException("File should not have less than "+ MAX_ROW +" rows");
 
         } catch (FileNotFoundException e) {
             throw new DataSourceException("File not found");
@@ -54,7 +57,7 @@ public class FileDataSource implements DataSource<Account> {
         account.setAccountNumber(p[3].trim());
 
         String errorCode = context.execute(account);
-        if (errorCode != null) throw new RuntimeException("Account number " + account.getAccountNumber() + "has error: " + errorCode);
+        if (errorCode != null) throw new RuntimeException("Account number " + account.getAccountNumber() + " has error: " + errorCode);
 
         return account;
     }
