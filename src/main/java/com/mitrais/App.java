54feca087caaf2a807e261a2c68e97cde040be98
HoangDH <huuhoang.dao@gmail.com>
2019-10-08 10:11:22 +0700

    Spring Boot Initialization

--- src/main/java/com/mitrais/App.java
+++ src/main/java/com/mitrais/App.java
@@ -1,21 +1,23 @@
 package com.mitrais;
 
-import com.mitrais.config.FileDataSource;
-import com.mitrais.model.TransactionHistory;
-import com.mitrais.repository.*;
 import com.mitrais.view.FileLoader;
 import com.mitrais.view.View;
 import com.mitrais.viewhandler.FrontController;
 import com.mitrais.viewhandler.FrontControllerImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
-public class App 
+
+@SpringBootApplication
+public class App
 {
     public static void main( String[] args )
     {
+        SpringApplication.run(App.class, args);
         View fileLoader = new FileLoader();
         fileLoader.display();
 
