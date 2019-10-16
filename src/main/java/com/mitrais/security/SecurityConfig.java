be7ec0f3e6278bd1f90e92473f409393fbca9ea8
HoangDH <huuhoang.dao@gmail.com>
2019-10-16 16:59:34 +0700

    File Upload Fixed

--- src/main/java/com/mitrais/security/SecurityConfig.java
+++ src/main/java/com/mitrais/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mitrais.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,17 +30,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
                     .antMatchers(
                             "/",
+                            "/file_upload",
                             "/static/**",
                             "/js/**",
                             "/css/**",
                             "/img/**",
                             "/webjars/**").permitAll()
-                    .antMatchers("/user/**").hasRole("USER")
-                    .anyRequest().authenticated()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                     .loginPage("/login")
