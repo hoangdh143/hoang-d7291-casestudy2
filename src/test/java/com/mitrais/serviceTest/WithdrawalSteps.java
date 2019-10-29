64d2f50d6e095032a943f6d2e0ce4d4969ae36c7
HoangDH <huuhoang.dao@gmail.com>
2019-10-29 14:58:35 +0700

    Cucumber Initialization

--- src/test/java/com/mitrais/serviceTest/WithdrawalSteps.java
+++ src/test/java/com/mitrais/serviceTest/WithdrawalSteps.java
@@ -1,26 +1,46 @@
 package com.mitrais.serviceTest;
 
-import com.mitrais.model.Account;
-import com.mitrais.repository.AccountRepository;
-import com.mitrais.service.AccountService;
+import cucumber.api.java.Before;
 import cucumber.api.java8.En;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static io.restassured.RestAssured.given;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 public class WithdrawalSteps implements En {
+    @Autowired
+    private WebApplicationContext wac;
+
     private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 
     public WithdrawalSteps() {
         Given("^authenticated user has a balance of (\\d+)$", (Integer arg0) -> {
-            final RequestSpecification request = given();
-            Response response = request.accept(ContentType.JSON).post("http://localhost:8080/login");
+            mockMvc.perform(get("/login")).andExpect(status().isOk());
+            mockMvc.perform(post("/login")
+                    .param("accountNumber", "811069")
+                    .param("pin", "288818"))
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(MockMvcResultMatchers.redirectedUrl("/transaction"));
         });
-        When("^user performs a withdrawal of (\\d+)$", (Integer arg0) -> {
+        When("^user performs a withdrawal of (\\d+)$", (Integer amount) -> {
+            mockMvc.perform(post("/withdraw")
+                    .param("amount", amount.toString()))
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(redirectedUrl("/withdraw/summary"));
         });
         Then("^the balance is (\\d+)$", (Integer arg0) -> {
         });
